7ada0ffb9baa7248779102d78f9c18d45f21b62f
Aden Addis <adenwonde56@gmail.com>
2022-08-26 10:05:41 -0400

    about us

--- cafe/src/components/AboutUs.js
+++ cafe/src/components/AboutUs.js
@@ -7,7 +7,11 @@ function AboutUs () {
         <main > 
              <h1 className="block"> Welcome to Double A Cafe </h1>
              <div className="block-about"> 
-                <p> YOOOOO </p>
+                <p> Double A Cafe was founded by two life long friends by the names of Aden and Alvin, hence where the name "Double A" comes from in 2019.
+                    Here we strive to give you a safe space to come and relax at our lounge area doing your work while you sip on our exquisite drinks. When in a rush, you can order online for pickup. New York is busy, but never too busy for some coffee ! </p>
+             </div>
+             <div className="block-aboutt"> 
+                <p> We are located on: 60 W 129th St, New York, NY 10027   </p>
              </div>
         </main>
 
