f81baf007b52ade9d9242e601ff41cc2b6851679
Alvin <alvin.sanchez27@gmail.com>
2022-08-26 02:31:53 -0400

    added cart

--- cafe/src/components/Cart.js
+++ cafe/src/components/Cart.js
@@ -1,38 +1,49 @@
-import React, { useState } from "react";
-function Cart({coffee}){
-    const [cartItems, setCartItems] = useState([])
-    const onAdd = (coffee) => {
-        const exist = cartItems.find(x => x.id === coffee.id)
-        if(exist) {
-          setCartItems(cartItems.map(x => x.id === coffee.id ? {...exist, qty: exist.qty +1} : x
-            )
-          );
-        } else {
-          setCartItems([...cartItems, {...coffee, qty: 1}])
-        }
-      }
-    
+import React from "react";
+function Cart({cartItems, onAdd, onRemove}){
+    const itemsPrice = cartItems.reduce((a,c) => a + c.price * c.qty, 0)
+    const taxPrice = itemsPrice * 0.08;
+    const totalPrice = itemsPrice + taxPrice
     return(
         <aside className="block .col-1">
-            <h2>Cart Items
-            {coffee}
-            </h2>
-            <div>
-                {cartItems.length === 0 && <div>Cart Is Empty</div>}
-                {onAdd}
-            </div>
-            {cartItems.map((item) => {
-                <div key={item.id} class="row">
+            <h2>Cart Items</h2>
+            <div>{cartItems.length === 0 && <div>Cart Is Empty</div>}</div>
+            {cartItems.map((item) => (
+                <div key={item.id} className="row">
                     <div className="col-2">{item.name}</div>
-                <div className="col-2">
-                <button onClick={() => onAdd(item)} className="add">+</button>
-                {/* <button onClick={() => onRemove(item)} className="remove">-</button> */}
+                    <div className="col-2">
+                        <button onClick={() => onAdd(item)} className="add">+</button>
+                        <button onClick={() => onRemove(item)} className="remove">-</button>
+                    </div>
+                    <div className="col-2 text-right">
+                        {item.qty} x ${item.price.toFixed(2)}
+                    </div>
                 </div>
-                <div className="col-2 text-right">
-                    {item.qty} x ${item.price.toFixed(2)}
+            ))}
+            {cartItems.length !== 0 && (
+                <>
+                <hr></hr>
+                <div className="row">
+                    <div className="col-2">Items Price</div>
+                    <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
                 </div>
-            </div>
-            })}
+                <div className="row">
+                    <div className="col-2">Items Tax</div>
+                    <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <strong>Total Price </strong>
+                        </div>
+                    <div className="col-1 text-right">${totalPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                    <button onClick={() => alert("Thank you for your order")}>
+                        Checkout
+                    </button>
+                </div>
+                </>
+    
+            )}
         </aside>
     )
 }
